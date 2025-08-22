7dfad897cc87cc53b1cf9bd214e0e4c306454c2a
Cole Joseph Roberts <161370035+cjr03@users.noreply.github.com>
2025-08-22 12:06:06 -0700

    Update isVectorInNullSpaceOfMatrix.py

--- isVectorInNullSpaceOfMatrix.py
+++ isVectorInNullSpaceOfMatrix.py
@@ -7,7 +7,7 @@ def isVectorInNullSpaceOfMatrix(matrix, vector):
 
 	for row in range(rows):
 		sum = 0
-		for col in range(cols)):
+		for col in range(cols):
 			num += matrix[row][col] * vector[col]
 		if sum != 0:
 			return False
